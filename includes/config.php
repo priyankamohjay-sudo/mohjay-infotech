3782d456bd1fb09af9f8920a9b100ceb3d3c7493
Priyanka Badoni <priyanka.mohjay@gmail.com>
2026-03-23 14:29:56 +0530

    update db and config

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,10 @@
 <?php
 
-// Base URL
-define('BASE_URL', 'http://localhost/mohjay-infotech/');
+// Base URL for server
+//define('BASE_URL', 'http://localhost/mohjay-infotech/');
+
+// Base URL for localhost
+define('BASE_URL', 'https://www.mohjayinfotech.com/');
 
 // Include database connection
 require_once __DIR__ . '/db.php';
