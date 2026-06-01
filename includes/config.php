a46e13e3771fded1db4a3c52e21e7b8270ba26b3
Ayush <ayush.mohjay@gmail.com>
2026-06-01 15:11:36 +0530

    Carrer form has been working properly and tested by me

--- includes/config.php
+++ includes/config.php
@@ -8,10 +8,10 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 // Base URL for local
-define('BASE_URL', 'http://localhost/mohjay-infotech/');
+// define('BASE_URL', 'http://localhost/mohjay-infotech/');
 
 // Base URL for server
-//define('BASE_URL', 'https://www.mohjayinfotech.com/');
+define('BASE_URL', 'https://www.mohjayinfotech.com/');
 
 // SMTP Configuration (Loaded from .env)
 define('SMTP_HOST', $_ENV['SMTP_HOST'] ?? 'smtpout.secureserver.net');
